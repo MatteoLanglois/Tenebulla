fb4666b7f51478f290a6b7e31c344f9ad69cd60e
Drenmax7 <92438713+Drenmax7@users.noreply.github.com>
2025-01-25 15:35:26 +0100

    ajout murene

---
+++ Assets/MureneDeplacement.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class MureneDeplacement : MonoBehaviour
+{
+    //longueur de l'extention de la murene
+    public float longueur = 3;
+    //position de depart de la murene
+    public float depart = 0;
+
+    //vitesse d'extention de la murene
+    public float speed = 1;
+
+    //temps passé par la murene dans son tuyau avant de sortir
+    public float cooldown = 1;
+    //temps passé par la murene avant de se retracter
+    public float attente = 2;
+
+    public GameObject spawner;
+
+    private bool extention = false;
+    private Vector3 debutExtention;
+    private Vector3 avance;
+    private float timer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        debutExtention = transform.position;
+        initialiseAvance();
+    }
+    private void OnValidate()
+    {
+        initialiseAvance();
+    }
+
+    void initialiseAvance()
+    {
+        Debug.Log(speed);
+        Vector3 direction = new Vector3(speed, 0, 0);
+        float angleZ = spawner.transform.eulerAngles.z;
+        Quaternion rotation = Quaternion.Euler(0, 0, angleZ);
+        avance = -(rotation * direction);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        { 
+            timer -= Time.deltaTime;
+            if (timer < 0) timer = 0;
+            return;
+        }
+
+        if (extention)
+        {
+            transform.position += avance * Time.deltaTime;
+        }
+        else
+        {
+            transform.position -= avance * Time.deltaTime;
+        }
+
+        Vector3 ecart = transform.position-debutExtention;
+        float angleZ = spawner.transform.eulerAngles.z;
+        Quaternion rotation = Quaternion.Euler(0, 0, -angleZ);
+        float avancement = -(rotation * ecart).x;
+
+        if (avancement < 0)
+        {
+            extention = true;
+            transform.position = debutExtention;
+            timer = cooldown;
+        }
+        if (avancement > longueur) 
+        {
+            extention = false;
+            timer = attente;
+
+        }
+    }
+}
