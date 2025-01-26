c6d91563d8604c3afc9bc084af2320be15fea01d
Léo T <leo.thomas2003@orange.fr>
2025-01-26 10:48:50 +0100

    Ecran de Mort fix + ajout zone fin

---
+++ Assets/Script/GotoEndGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GotoEndGame : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Assurez-vous que votre joueur a le tag "Player"
+        {
+            //SceneManager.LoadScene("EndGameScene");
+        }
+    }
+}
