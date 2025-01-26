d18aa0ab04299624245246113af0f7f209c60b4b
matteo langlois <langlois.matteo49@gmail.com>
2025-01-26 13:31:19 +0100

    Added the shell

---
+++ Assets/Script/Shell.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Script
+{
+    public class Shell : MonoBehaviour
+    {
+        public int shellId;
+
+        public int GetShellId()
+        {
+            return shellId;
+        }
+    }
+}
